6ede518bb7a32105dd4248f10dcfddf882018b6c
KingTiger166 <148370499+kingtiger166@users.noreply.github.com>
2025-05-23 12:51:19 -0300

    Add files via upload

--- Itens/script.js
+++ Itens/script.js
@@ -1,3 +1,11 @@
 import { Ripple, initMDB } from "mdb-ui-kit";
 
 initMDB({ Ripple });
+
+document.querySelectorAll('[tabindex]').
+forEach(el => {
+    el.addEventListener('focus', () => {
+        el.scrollIntoView({behavior:
+'smooth', block: 'center'});
+    });
+});
